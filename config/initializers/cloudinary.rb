447ed1f25a8de00e640409e328dfaa22de7acb84
oleg <krasno1986@gmail.com>
2021-04-04 16:50:22 +0300

    add cloudinary settings

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dd69eghxe'
+  config.api_key = '773622867178954'
+  config.api_secret = 'pPl3NLzglIqsUcZKbtYYuCWuB-g'
+  config.enhance_image_tag = true
+  config.static_file_support = true
+end
